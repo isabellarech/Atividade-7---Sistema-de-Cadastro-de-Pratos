2420955f4cb0e700894d8a4809121078dc69e3ca
sophia hardt <sophia_hardt@estudante.sesisenai.org.br>
2026-08-19 09:17:22 -0300

    add - adicionei o banco de dados

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE sistema_cadastro_pratos;
+USE sistema_cadastro_pratos;
+
+CREATE TABLE pratos (
+    id_prato INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(50),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (id_usuario) REFERENCES usuario_responsavel(id_prato)
+);
