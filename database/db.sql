ce4c7d7d11db0b365f4cfe65d6e11477d06b138e
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-19 11:26:23 -0300

    add - arrumei o banco de dados

--- database/db.sql
+++ database/db.sql
@@ -1,5 +1,12 @@
-CREATE DATABASE sistema_cadastro_pratos;
+CREATE DATABASE IF NOT EXISTS sistema_cadastro_pratos;
 USE sistema_cadastro_pratos;
+
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome_usuario VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE pratos (
     id_prato INT AUTO_INCREMENT PRIMARY KEY,
@@ -7,13 +14,7 @@ CREATE TABLE pratos (
     descricao TEXT,
     preco DECIMAL(10, 2) NOT NULL,
     categoria VARCHAR(50),
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome_usuario VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (id_usuario) REFERENCES usuario_responsavel(id_prato)
+    id_usuario INT NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
